6cf2f7bb148ab5e3f0ae2b813d069081fdf5cc29
SMAUG\Pete <pwbrophy@gmail.com>
2021-05-10 21:53:36 +0100

    messing about

--- phaselegs_03.py
+++ phaselegs_03.py
@@ -70,6 +70,8 @@ def robot_walk_forwards():
 
     number_of_servos = 12
     global robot_is_walking
+    robot_is_walking = False
+
     # Define hip positions
     hip_center = 90
     hip_forwards = 60
@@ -173,6 +175,7 @@ def robot_walk_forwards():
                     # Calculate and move the knees
                     for servo in knee_set_a:
                         angle_for_this_servo = knee_curve_a.ease(current_time_from_zero)
+                        print("angle problem is "angle_for_this_servo)
                         kit.servo[servo].angle = angle_for_this_servo
                     for servo in knee_set_b:
                         angle_for_this_servo = knee_curve_b.ease(current_time_from_zero)
