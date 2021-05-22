867293959819144880972d38aaa936061253650f
SMAUG\Pete <pwbrophy@gmail.com>
2021-05-22 16:48:16 +0100

    messing about

--- robot_leg_functions.py
+++ robot_leg_functions.py
@@ -24,8 +24,7 @@ def generate_servo_movement_curve(this_servo_current_position,
 
     set_a_phase = phase
     set_b_phase = (phase + phase_offset) % 4
-    print(hip_target_position_a)
-    print(hip_target_position_b)
+
     hip_target_position_a = hip_target_position_phase[set_a_phase]
     hip_target_position_b = hip_target_position_phase[set_b_phase]
     knee_target_position_a = knee_target_position_phase[set_a_phase]
