62028dc62d0f3911c45b4ee62298b12b4e7d27c8
SMAUG\Pete <pwbrophy@gmail.com>
2021-05-24 22:28:39 +0100

    Video streaming demo example

--- robot.py
+++ robot.py
@@ -19,6 +19,18 @@ global moving_speed
 moving_speed = 0
 
 
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    camera.rotation = 180
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
+
 class MyApp(App):
     def __init__(self, *args):
         super(MyApp, self).__init__(*args)
