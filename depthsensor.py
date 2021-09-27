ab40f8fa22b075e697ac641fb55175532400def1
SMAUG\Pete <pwbrophy@gmail.com>
2021-09-27 21:41:41 +0100

    Video streaming demo example

--- depthsensor.py
+++ depthsensor.py
@@ -13,7 +13,6 @@ import adafruit_vl53l0x
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-sensor.setMeasurementTimingBudget(200000);
 
 # Optionally adjust the measurement timing budget to change speed and accuracy.
 # See the example here for more details:
