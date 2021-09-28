a5bfa83d4dfb4a52437de099fc83e027f70d26d9
SMAUG\Pete <pwbrophy@gmail.com>
2021-09-28 12:04:44 +0100

    Video streaming demo example

--- depthsensor.py
+++ depthsensor.py
@@ -20,10 +20,10 @@ vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 # For example a higher speed but less accurate timing budget of 20ms:
 # vl53.measurement_timing_budget = 20000
 # Or a slower but more accurate timing budget of 200ms:
-vl53.measurement_timing_budget = 200000
+# vl53.measurement_timing_budget = 200000
 # The default timing budget is 33ms, a good compromise of speed and accuracy.
 
 # Main loop will read the range and print it every second.
 while True:
     print("Range: {0}mm".format(vl53.range))
-    time.sleep(0.1)
+    time.sleep(0.01)
