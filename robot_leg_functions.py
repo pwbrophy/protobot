04d0a1b53459966fada2fad799b1027088965461
SMAUG\Pete <pwbrophy@gmail.com>
2023-04-25 22:42:34 +0100

    Video streaming demo example

--- robot_leg_functions.py
+++ robot_leg_functions.py
@@ -26,6 +26,9 @@ def generate_servo_movement_curve(this_servo_current_position,    # Current posi
     set_a_phase = phase
     set_b_phase = (phase + phase_offset) % 4
 
+    print("set b phase "+str(set_a_phase))
+    print("set a phase "+str(set_b_phase))
+
     hip_target_position_a = hip_target_position_phase[set_a_phase]
     hip_target_position_b = hip_target_position_phase[set_b_phase]
     knee_target_position_a = knee_target_position_phase[set_a_phase]
