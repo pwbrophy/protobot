3572940b0598bdf935316d4b39223db1ccb8cf2c
SMAUG\Pete <pwbrophy@gmail.com>
2023-04-25 22:05:07 +0100

    Video streaming demo example

--- robot_walking.py
+++ robot_walking.py
@@ -141,15 +141,15 @@ def turn_on_robot_locomotion():
     walk_forwards_knee_smooth = [1, 0, 0, 0]
 
     # Stopping gait raised leg (THESE GOT MIXED UP WHEN THE SERVOS FLIPPED)
-    stop_down_hip_phase_order = [-1, hip_center, hip_center, hip_center]
-    stop_down_knee_phase_order = [knee_down, knee_center, knee_center, knee_center]
+    stop_down_hip_phase_order = [-1, -1, -1, -1]
+    stop_down_knee_phase_order = [-1, -1, -1, -1]
     # Smoothing 0 - ease both, 1 - ease out from current, 2 = ease in to next, 3 = linear
     stop_down_hip_smooth = [0, 0, 0, 0]
     stop_down_knee_smooth = [0, 0, 0, 0]
 
     # Stopping gait down leg (THESE GOT MIXED UP WHEN THE SERVOS FLIPPED)
-    stop_raised_hip_phase_order = [-1, -1, -1, hip_center]
-    stop_raised_knee_phase_order = [-1, -1, knee_down, knee_center]
+    stop_raised_hip_phase_order = [-1, -1, -1, -1]
+    stop_raised_knee_phase_order = [-1, -1, -1, -1]
     # Smoothing 0 - ease both, 1 - ease out from current, 2 = ease in to next, 3 = linear
     stop_raised_hip_smooth = [0, 0, 0, 0]
     stop_raised_knee_smooth = [0, 1, 2, 0]
