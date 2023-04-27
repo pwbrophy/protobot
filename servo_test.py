58b92558494e1fff5f10adf2124daf9e69a78b31
SMAUG\Pete <pwbrophy@gmail.com>
2023-04-27 20:41:40 +0100

    Servo Tester

---
+++ servo_test.py
@@ -0,0 +1,18 @@
+from adafruit_servokit import ServoKit
+import time
+
+kit = ServoKit(channels=16)
+
+servos = []
+
+for servo in range(12):
+    servos.append(servo)
+
+for servo in servos:
+    kit.servo[servo].angle = 0
+    for angle in range(0, 180, 5):
+        kit.servo[servo].angle = angle
+        time.sleep(0.05)
+    time.sleep(2)
+    kit.servo[servo].angle = None
+
